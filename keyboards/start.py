66cc723504553ed4613f41ec57991f58b4b25423
sweankss <156759254+sweankss@users.noreply.github.com>
2024-05-03 17:18:27 +0300

    cgfh

---
+++ keyboards/start.py
@@ -0,0 +1,15 @@
+"""Клавиатуры для команды /start"""
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+kb_next_btn = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(
+            text='Вперед',
+            callback_data='next')]])
+
+kb_back_btn = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data='back')]])
